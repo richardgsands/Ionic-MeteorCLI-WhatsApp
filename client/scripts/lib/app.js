a260ddf15fb9df9bd03b2c7df803785863683ecf
Richard Sands <rsands@oxts.com>
2017-08-01 14:59:22 +0100

    step3 (step4 in progress)

--- client/scripts/lib/app.js
+++ client/scripts/lib/app.js
@@ -1,8 +1,6 @@
 // Libs
 import 'angular-animate';
 import 'angular-meteor';
-import 'angular-meteor-auth';
-import 'angular-moment';
 import 'angular-sanitize';
 import 'angular-ui-router';
 import 'ionic-scripts';
@@ -13,51 +11,30 @@ import { Meteor } from 'meteor/meteor';
 // Modules
 import ChatsCtrl from '../controllers/chats.controller';
 import ChatCtrl from '../controllers/chat.controller';
-import ConfirmationCtrl from '../controllers/confirmation.controller';
-import LoginCtrl from '../controllers/login.controller';
-import NewChatCtrl from '../controllers/new-chat.controller';
-import ProfileCtrl from '../controllers/profile.controller';
-import SettingsCtrl from '../controllers/settings.controller';
-import InputDirective from '../directives/input.directive';
 import CalendarFilter from '../filters/calendar.filter';
-import ChatNameFilter from '../filters/chat-name.filter';
-import ChatPictureFilter from '../filters/chat-picture.filter';
-import NewChatService from '../services/new-chat.service';
-import Routes from '../routes';
+import RoutesConfig from '../routes';
 
 const App = 'Whatsapp';
 
 // App
 Angular.module(App, [
-  'angular-meteor',
-  'angular-meteor.auth',
-  'angularMoment',
-  'ionic'
+    'angular-meteor',
+    'ionic'
 ]);
 
 new Loader(App)
-  .load(ChatsCtrl)
-  .load(ChatCtrl)
-  .load(ConfirmationCtrl)
-  .load(LoginCtrl)
-  .load(NewChatCtrl)
-  .load(ProfileCtrl)
-  .load(SettingsCtrl)
-  .load(InputDirective)
-  .load(CalendarFilter)
-  .load(ChatNameFilter)
-  .load(ChatPictureFilter)
-  .load(NewChatService)
-  .load(Routes);
+    .load(ChatsCtrl)
+    .load(ChatCtrl)
+    .load(CalendarFilter)
+    .load(RoutesConfig);
 
 // Startup
 if (Meteor.isCordova) {
-  Angular.element(document).on('deviceready', onReady);
-}
-else {
-  Angular.element(document).ready(onReady);
+    Angular.element(document).on('deviceready', onReady);
+} else {
+    Angular.element(document).ready(onReady);
 }
 
 function onReady() {
-  Angular.bootstrap(document, [App]);
+    Angular.bootstrap(document, [App]);
 }
